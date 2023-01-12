54c8a6d4fc264805a99377d39fc64833bcee7ac4
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-13 00:31:48 +0530

    navbar layout for app page

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import {
+  Flex,
+  useColorModeValue,
+  Avatar,
+  MenuList,
+  Box,
+  Stack,
+  Button,
+  Menu,
+  MenuButton,
+  Center,
+  MenuDivider,
+  MenuItem,
+  Input,
+  useColorMode,
+  InputGroup,
+  InputRightElement,
+  Card,
+  CardBody,
+} from "@chakra-ui/react";
+import { SunIcon, MoonIcon, SearchIcon, BellIcon } from "@chakra-ui/icons";
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const [searchText, setSearchText] = useState();
+  return (
+    <>
+      <Box
+        bg={useColorModeValue("white", "gray.900")}
+        px={4}
+        w="100%"
+        shadow="lg"
+      >
+        <Flex
+          h={16}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          minWidth="max-content"
+          gap="2"
+        >
+          <Box fontSize={"2xl"} fontWeight="bold">
+            PeerLink
+          </Box>
+          <Box display={{ base: "none", md: "inline" }}>
+            <InputGroup>
+              <Input
+                placeholder="Search User"
+                w={{ base: "0px", md: "500px" }}
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+              />
+              <InputRightElement h={"full"}>
+                <Button variant={"ghost"}>
+                  <SearchIcon />
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+            {searchText && searchText.length >= 1 && (
+              <>
+                <Box
+                  position="absolute"
+                  w={{ base: "0px", md: "500px" }}
+                  marginTop="2px"
+                >
+                  <Card>
+                    <CardBody>Loading...</CardBody>
+                  </Card>
+                </Box>
+              </>
+            )}
+          </Box>
+          <Flex alignItems={"center"} justifyContent={"space-between"} gap={5}>
+            <Box
+              cursor="pointer"
+              bg={useColorModeValue("gray.200", "gray.600")}
+              w={8}
+              h={8}
+              rounded={"full"}
+              display="flex"
+              alignItems={"center"}
+              justifyContent={"center"}
+            >
+              <BellIcon w={5} h={5} />
+            </Box>
+            <Box
+              onClick={toggleColorMode}
+              cursor="pointer"
+              bg={useColorModeValue("gray.200", "gray.600")}
+              w={8}
+              h={8}
+              rounded={"full"}
+              display="flex"
+              alignItems={"center"}
+              justifyContent={"center"}
+            >
+              {colorMode === "light" ? (
+                <MoonIcon w={5} h={5} />
+              ) : (
+                <SunIcon w={5} h={5} />
+              )}
+            </Box>
+            <Stack direction={"row"} spacing={7}>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar size={"sm"} name="Peer Link" bg={"gray.200"} />
+                </MenuButton>
+                <MenuList alignItems={"center"}>
+                  <Center>
+                    <Avatar size={"xl"} name="Peer Link" />
+                  </Center>
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <MenuDivider />
+                  <MenuItem>Account</MenuItem>
+                  <MenuItem>Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;
--- client/src/layouts/AppLayout.jsx
+++ client/src/layouts/AppLayout.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "./../components/Navbar/Navbar";
 const AppLayout = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
   );
