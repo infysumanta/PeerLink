ebf1969a2c60926842ea27dc809a4e2eb4e326ca
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-12 21:38:21 +0530

    installed chakra ui and work on it

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
+import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
