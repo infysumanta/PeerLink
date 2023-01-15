617525c8a50845f6acb9c4d785dd8c1a661bd4bd
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-15 16:24:18 +0530

    path must be absolute or specify root to res.sendFile #1 solve

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(logger("common"));
-app.use(express.static(path.join("client", "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use("/api", require("./routes/routes"));
 
 /* This is a route handler. It is a function that is called when a request is made to the specified route. */
 app.get("*", (_, res) => {
-  res.sendFile(path.join("client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 /* Error Handling middleware function that is called when a request is make a error */
